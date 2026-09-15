c90abf980a8f980123ed2f6227ed591d5c8bc358
jayahopkins1802-commits <jayahopkins1802@gmail.com>
2026-09-15 05:55:25 +0100

    Fix UserButton type error by removing unsupported afterSignOutUrl prop

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar() {
         </SignedOut>
         
         <SignedIn>
-          <UserButton afterSignOutUrl="/" />
+          <UserButton />
         </SignedIn>
       </div>
     </nav>
